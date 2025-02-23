e689d57471297447179282a34c0cde0f5b910580
oenryque <juan.curbage@gmail.com>
2025-02-23 20:49:55 -0300

    Update Hero section and metadata with dynamic site data and image carousel

--- pages/template-base-academia/app/layout.tsx
+++ pages/template-base-academia/app/layout.tsx
@@ -1,10 +1,26 @@
 import type { Metadata } from 'next'
 import './globals.css'
-
+import siteData from '../data/siteData.json'
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: siteData.nome,
+  description: siteData.descricao,
+  generator: 'by Atipc - Soluções em Tecnologia',
+  openGraph: {
+    title: siteData.nome,
+    description: siteData.descricao,
+    images: [{
+      url: siteData.logo, // Certifique-se que existe uma propriedade logo no siteData
+      width: 1200,
+      height: 630,
+      alt: siteData.nome
+    }]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteData.nome,
+    description: siteData.descricao,
+    images: [siteData.logo], // Mesma imagem do OpenGraph
+  }
 }
 
 export default function RootLayout({
